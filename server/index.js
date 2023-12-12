e4283facc85c4ba2a3c6ffd1826d58ade30df085
reizellnn <reizellnn>
2023-12-12 13:09:25 +0800

    asd

--- server/index.js
+++ server/index.js
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use("/api", dashboardRoutes);
 
 app.listen(7061, () => {
-    console.log("server has started on port 5000");
+    console.log("server has started on port 7061");
 });
